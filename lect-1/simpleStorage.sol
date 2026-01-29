c9ffa5c2a15a26bcf051aec8cd59d0bf1be77839
uzumaki-ak <anikydv38@gmail.com>
2026-01-30 03:35:18 +0530

    learned data stru,mapping,visibility specifier,remix storage func

--- lect-1/simpleStorage.sol
+++ lect-1/simpleStorage.sol
@@ -8,11 +8,43 @@ contract  SimpleStorage {
 
     //uint256 favoriteNumber=0 or favoriteNumber; is same //as its initialized with 0 
 
+     //we can use "public" keyword to display fav no as this is internal by default 
 
-    uint256 favoriteNumber;
+    uint256 public favoriteNumber;
     //function in solidity
-    function store (uint256 _favoriteNumber) public {
-      favoriteNumber=_favoriteNumber;
-      
+  function store (uint256 _favoriteNumber) public {
+    favoriteNumber=_favoriteNumber;
+      uint testvar =29;
     }
+  function retrieve()  public view returns(uint256) {
+    return favoriteNumber;
+ }
+ //this is wrong as pure does not even allow us to read another state but view does
+  function retrieve()  public pure returns(uint256) {
+    return favoriteNumber;
+ }
+
+ function somtething public {
+  //this is wromg as we can not access func outside 
+  testvar=7;
+  //but thuis is correct as fav is inside SimpleStorage
+  favoriteNumber=78
+ }
+
+       struct Person{
+        uint256 favoriteNumber;
+        string name;
+      }
+
+    //   Person public ak = Person({
+    //     favoriteNumber:7,name: "ani"
+    //   });
+
+    //creating dynamc list 
+    Person[] public listOfPeople;
+   function addPerson(string memory _name,uint256 _favoriteNumber) public {
+    listOfPeople.push(Person(_favoriteNumber,_name));
+   }
+
+
 }
