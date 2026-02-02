fb789b1e34e6c2bd7cda05c7375c4dd1aef72f75
uzumaki-ak <anikydv38@gmail.com>
2026-02-03 02:35:21 +0530

    learned to call a func in 2nd file of another file

--- lect-1/StorageFactory.sol
+++ lect-1/StorageFactory.sol
@@ -1,43 +1,27 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.24;
 
-contract SimpleStorage {
-    // State variables
-    uint256 public favoriteNumber; // Stores a single favorite number
-    struct Person { // Defines a Person struct
-        uint256 favoriteNumber;
-        string name;
-    }
-    Person[] public listOfPeople; // Dynamic array of Person structs
-    mapping(string => uint256) public nameToFavoriteNumber; // Maps names to favorite numbers
-
-    // Stores a favorite number
-    function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
-    }
-
-    // Retrieves the stored favorite number
-    function retrieve() public view returns(uint256) {
-        return favoriteNumber;
-    }
-
-    // Adds a person to the list and updates the mapping
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        listOfPeople.push(Person(_favoriteNumber, _name));
-        nameToFavoriteNumber[_name] = _favoriteNumber;
-    }
-
-    
-}
-
+import {SimpleStorage} from'./simpleStorage.sol';
 
 contract StorageFactory {
-
-SimpleStorage public simpleStorage;
+//this stores address
+SimpleStorage[] public listOfSimpleStorageContract;
 
 
     function createSimpleStorageFActory() public {
         //this deploys another smart contract the  new word
-    simpleStorage = new SimpleStorage();
+    SimpleStorage newSimpleStorageContract = new SimpleStorage();
+    listOfSimpleStorageContract.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber)public{
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContract[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContract[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
     }
 }
