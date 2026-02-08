277bcc11a98ab132226a810cd17c9f3dc43dd215
uzumaki-ak <anikydv38@gmail.com>
2026-02-09 00:58:52 +0530

    done with fundme next oracle

--- fund-me/Fundme.sol
+++ fund-me/Fundme.sol
@@ -4,6 +4,7 @@
 pragma solidity ^0.8.24;
 
 contract FundMe {
+    // uint256 public myval=1;
     function fund() public payable {
         //alllow user ot send money
         //have a minimum usd dol  sent
@@ -13,6 +14,8 @@ contract FundMe {
         //1e17 is 0.1 eth
         // require is used to set min amout which is needed ot be  sent
          // if the amount is less than 1e18 it will revert
+         // msg.value is the amount of eth sent by the user
+        //  myval = myval+2;
         require(msg.value > 1e18, "minimum amount required");
     }
 
