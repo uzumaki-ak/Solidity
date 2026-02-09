81aa45f707b163a5340f01e80884a5ba9c4bb585
uzumaki-ak <anikydv38@gmail.com>
2026-02-10 02:52:06 +0530

    learned interfaces in solidity and deployed a contract

--- fund-me/Fundme.sol
+++ fund-me/Fundme.sol
@@ -3,8 +3,31 @@
 
 pragma solidity ^0.8.24;
 
+
+// solhint-disable-next-line interface-starts-with-i
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
+
+
 contract FundMe {
+    
     // uint256 public myval=1;
+    uint256 public minimumUSD = 5;
     function fund() public payable {
         //alllow user ot send money
         //have a minimum usd dol  sent
@@ -20,4 +43,10 @@ contract FundMe {
     }
 
 // function withdraw() public{} 
+
+ function  getPrice() public {}
+ function getConversionRate() public {}
+ function getVersion() public view returns (uint256) {
+return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+ }
 }
