f310df2adabe1d5d503cf66fa5c604d5f82320ad
uzumaki-ak <anikydv38@gmail.com>
2026-02-10 03:09:32 +0530

    used npm packg instead of writing code

--- fund-me/Fundme.sol
+++ fund-me/Fundme.sol
@@ -3,24 +3,26 @@
 
 pragma solidity ^0.8.24;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 
 // solhint-disable-next-line interface-starts-with-i
-interface AggregatorV3Interface {
-  function decimals() external view returns (uint8);
+// interface AggregatorV3Interface {
+//   function decimals() external view returns (uint8);
 
-  function description() external view returns (string memory);
+//   function description() external view returns (string memory);
 
-  function version() external view returns (uint256);
+//   function version() external view returns (uint256);
 
-  function getRoundData(
-    uint80 _roundId
-  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+//   function getRoundData(
+//     uint80 _roundId
+//   ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
 
-  function latestRoundData()
-    external
-    view
-    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-}
+//   function latestRoundData()
+//     external
+//     view
+//     returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+// }
 
 
 
